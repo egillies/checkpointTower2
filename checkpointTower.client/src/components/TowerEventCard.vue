368da4dd2f28ecc69cc92fa13ac1bee802e5929d
Emily <gilliesemily@yahoo.com>
2023-07-28 00:08:17 -0600

    .

--- checkpointTower.client/src/components/TowerEventCard.vue
+++ checkpointTower.client/src/components/TowerEventCard.vue
@@ -5,22 +5,12 @@
 
             <div class="text-center" p-2>
                 <h2> {{ towerEventProp.name }}</h2>
-                <!-- <p> hosted by: {{ towerEventProp.creatorId }}</p> -->
                 <div>
                     <i class="mdi mdi-account"></i>
                     <p>{{ towerEventProp.capacity }}</p>
                 </div>
-                <p> {{ towerEventProp.description }}</p>
-                <p> {{ towerEventProp.location }}</p>
-                <p> {{ towerEventProp.startDate }}</p>
 
-                <div v-if="account.id == towerEventProp.creatorId">
-                    <button class="btn btn-success ms-2" @click="archiveTowerEvent()" type="button">
-                        <i class="mdi mdi-close-circle text-danger">Delete Event</i>
-                    </button>
-                </div>
             </div>
-
         </div>
     </router-link>
 </template>
@@ -31,8 +21,8 @@ import { TowerEvent } from '../models/TowerEvent.js';
 import { useRoute } from 'vue-router';
 import { computed } from 'vue';
 import { AppState } from '../AppState.js';
-import Pop from '../utils/Pop.js';
-import { towerEventsService } from '../services/TowerEventsService.js';
+// import Pop from '../utils/Pop.js';
+// import { towerEventsService } from '../services/TowerEventsService.js';
 
 
 
@@ -46,25 +36,13 @@ export default {
             route,
             account: computed(() => AppState.account),
             towerEvent: computed(() => AppState.activeTowerEvent),
-            async archiveTowerEvent() {
-                try {
-                    const wantsToArchive = await Pop.confirm()
 
-                    if (!wantsToArchive) {
-                        return
-                    }
-
-                    const towerEventId = route.params.eventId
-                    await towerEventsService.archiveTowerEvent(towerEventId)
-                } catch (error) {
-                    Pop.error(error.message)
-                }
-            }
         }
     }
 
 }
 </script>
+
 
 
 <style lang="scss" scoped></style>
--- checkpointTower.client/src/pages/TowerEventDetailsPage.vue
+++ checkpointTower.client/src/pages/TowerEventDetailsPage.vue
@@ -1,27 +1,34 @@
 <template>
-    <div class="container-fluid">
+    <div v-if="towerEvent" class="container-fluid">
         <div class="row">
             <!--SECTION TowerEvent details-->
-            <div v-if="towerEvent" class="col-3">
+            <div class="col-3">
                 <div class="d-flex justify-content-between align-items-start">
                     <img :src="towerEvent.coverImg" :alt="towerEvent.name" class="img-fluid">
                 </div>
             </div>
 
-            <div class="rounded bg-warning p-1 light-shadow">
+            <div class="rounded p-1 light-shadow">
                 <p class="fs-4 fw-bold">
                     {{ towerEvent?.name }}
                 </p>
-                <p class="fs-4">
-                    hosted by: {{ towerEvent?.creator?.name }}
-                </p>
+                <div class="d-flex align-items-start">
+
+                    <p class="fs-4">
+                        {{ towerEvent?.startDate }}
+                        {{ towerEvent?.type }}
+                        {{ towerEvent?.description }}
+                    </p>
+                </div>
+
+                <div class="pt-3">
+                    <button v-if="account?.id == towerEvent?.creatorId" :disabled="towerEvent.isCanceled == true"
+                        class="btn btn-danger" @click="archiveTowerEvent()" type="button">
+                        <i class="mdi mdi-close-circle text-dark">Delete Event</i>
+                    </button>
+                </div>
             </div>
 
-            <div class="pt-3">
-                <!--TODO disable or hide if user is not ticketholder or creator-->
-                <button v-if="towerEvent?.creatorId == account.id">Add Event</button>
-
-            </div>
         </div>
     </div>
 </template>
@@ -33,36 +40,57 @@ import { towerEventsService } from '../services/TowerEventsService.js';
 import Pop from '../utils/Pop.js';
 import { computed, onMounted, watchEffect } from 'vue';
 import { AppState } from '../AppState.js';
+import { TowerEvent } from '../models/TowerEvent.js';
 
 
 //NOTE these functions run on page load
 export default {
+
     setup() {
         const route = useRoute()
 
-        async function getTowerEventsById() {
+        async function getTowerEventsById(eventId) {
             try {
-                const towerId = route.params.eventId
-                await towerEventsService.getTowerEventsById(towerId)
+                const eventId = route.params.eventId
+                await towerEventsService.getTowerEventsById(eventId)
             } catch (error) {
                 Pop.error(error.message)
             }
         }
+
 
         // onMounted(()=>{
 
         // })
 
         watchEffect(() => {
-            getTowerEventsById()
+            getTowerEventsById(route.params.eventId)
+            // archiveTowerEvent()
         })
 
         return {
             towerEvent: computed(() => AppState.activeTowerEvent),
             account: computed(() => AppState.account),
+
+            async archiveTowerEvent() {
+                try {
+                    const wantsToArchive = await Pop.confirm()
+
+                    if (!wantsToArchive) {
+                        return
+                    }
+
+                    const towerEventId = route.params.eventId
+                    await towerEventsService.archiveTowerEvent(towerEventId)
+                } catch (error) {
+                    Pop.error(error.message)
+                }
+            }
         }
     }
+
 }
+
 </script>
 
 
