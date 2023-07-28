485fb46f671df2615a64a7d98af25865b438daba
Emily <gilliesemily@yahoo.com>
2023-07-28 13:31:15 -0600

    comment stuff

--- checkpointTower.client/src/services/CommentsService.js
+++ checkpointTower.client/src/services/CommentsService.js
@@ -3,6 +3,12 @@ import { Comment } from "../models/Comment.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 class CommentsService {
+
+    async getCommentsByTowerEventId(eventId) {
+        const res = await api.get(`api/events/${eventId}/comments`)
+        logger.log('[GETTING COMMENTS]', res.data);
+        AppState.comments = res.data.map(d => new Comment(d))
+    }
     async createComment(formData) {
         const res = await api.post('api/comments', formData)
         logger.log('[CREATING COMMENT]', res.data);
