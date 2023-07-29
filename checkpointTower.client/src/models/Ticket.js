b79aee6d2c056ea13781114494c31f6242b27350
Emily <gilliesemily@yahoo.com>
2023-07-28 22:53:58 -0600

    f

--- checkpointTower.client/src/models/Ticket.js
+++ checkpointTower.client/src/models/Ticket.js
@@ -1,12 +1,10 @@
-import { TowerEvent } from "./TowerEvent.js"
-
 export class Ticket {
     constructor(data) {
         this.id = data.id
         this.eventId = data.eventId
         this.accountId = data.accountId
         this.profile = data.profile
-        this.event = new TowerEvent(data.event)
+        this.event = data.event
     }
 }
 // let data = {
